9b989705b089aaabc24c3cb8700e95148bc31abe
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-23 20:50:59 +0500

    For loop in c++

---
+++ forloop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+for(int i=0;i<6;i++){
+	
+	cout<<i<< "\t"<<"pak\n";
+	cout<<"HELLO WORLD"<<endl;
+	
+}
+cout<<"    "<<endl;
+cout<<"    "<<endl;
+
+
+cout<<"Another loop"<<endl;
+
+cout<<"    "<<endl;
+cout<<"    "<<endl;
+
+
+for(int j=0;j<6;j++){
+	
+
+	cout<<"HELLO WORLD"<<endl;
+	
+}
+    return 0;
+}
+

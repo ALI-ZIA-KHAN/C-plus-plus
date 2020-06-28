bb4120450511a18403aa11a6445050ddaff66f93
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-28 22:21:16 +0500

    while loop with break keyword

---
+++ breakwhile.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+#include<conio.h>
+#include<string>
+
+using namespace std;
+int main(){
+int j=0;
+while(j<=10)
+{
+	
+	cout<<j<<"\n";
+	j++;
+    if(j==5)
+	{
+	 break;	
+		}	
+	
+}
+return 0;
+}
+
+
+
+

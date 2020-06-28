852e6fa037470f4b783cdf22a4bc836118e5f1d1
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-29 00:31:40 +0500

    simple function in c++

---
+++ simple function.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+#include<conio.h>
+#include<string>
+
+using namespace std;
+
+
+void function1(){
+	
+	cout<<"NED University"<<"\n";
+	
+	
+}
+void function2(){
+	
+	cout<<"karachi University"<<"\n";
+	
+	
+}
+
+int main(){
+  function1();
+  function2();
+  
+  
+  return 0;
+}
+
+
+

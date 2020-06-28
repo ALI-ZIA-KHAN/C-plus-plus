1bb52f0a737c51a271f9015833e35dbac741ad71
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-28 23:07:42 +0500

    DOWHILE LOOP ...one time must be executed

---
+++ dowhileloop.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<conio.h>
+#include<string>
+
+using namespace std;
+int main(){
+int j=10;
+do{
+	cout<<"The value of j is "<<j<<endl;
+	j++;
+	
+}
+  while(j<10);
+  return 0;
+}
+/*one time loop must be executed no matter if the condition is true or not*/
+
+/*
+  The value of j is 10 */
+
+
+

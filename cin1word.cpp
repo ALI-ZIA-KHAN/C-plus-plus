38d8ac21a71bfd0844eda8c7c51d92b00f620d93
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-28 22:01:48 +0500

    printing 1 word no matter u input how many(v. imp)

---
+++ cin1word.cpp
@@ -0,0 +1,18 @@
+/**for loop**/
+#include<iostream>
+#include<conio.h>
+#include<string>
+/**very very important*/
+/**cin considers space as terminating character , which means it willl
+display only a single word even if u type more than 1 word*/
+using namespace std;
+int main(){
+ string a;
+ cout<<"enter anything"<<endl;
+ cin>>a;
+ cout<<"see it has only printed\t"<<a<<endl;
+ 
+ return 0;
+ 
+ }
+

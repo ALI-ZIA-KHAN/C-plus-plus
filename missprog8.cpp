a5436ee96a77d3a6806f79533d1bf3fc9aecbe4e
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-06 22:44:19 +0500

    array with for loop

---
+++ missprog8.cpp
@@ -0,0 +1,28 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	/*can be like int a[]={,,,,} also*/
+	int a[5]={2,5,9,4,8};
+	
+	for(int i=0;i<5;i++){
+		
+		cout<<"now here we get is "<<a[i]<<endl;
+		
+	}
+cout<<"\n\n ANOTHER EXAMPLE \n\n"<<endl;	
+	string b[3]={"NED","FAST","KU"};
+	
+	for(int j=0;j<5;j++){
+		
+		cout<<"now here we get is "<<b[j]<<endl;
+		
+		}
+		return 0;
+		/*also running without return statement*/
+	
+	
+}
+

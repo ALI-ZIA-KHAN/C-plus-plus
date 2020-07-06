8b2a97101a49a31251fcc4e17d3de7a3ee3be46e
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-06 17:26:42 +0500

    if/else only

---
+++ missprog2 ifelse.cpp
@@ -0,0 +1,25 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ cout<<"enter no:"<<endl;
+ cin>>a;
+ /*cin wale statement men endl lgaya to nhi chla, hyaya to chlgya*/
+ 
+ 
+ /**== se compare krna*/
+ if(a==3){
+ 	
+ 	cout<<"your no. is three\n"<<endl;
+ 	
+ }
+ 
+ else{
+ 	cout<<"u entered someother no."<<endl;
+ }
+ 
+     return 0;
+ 
+}

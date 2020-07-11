d8e03bf68dfa9f3cc82b78ec0a3e26d92c26a092
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-11 13:16:06 +0500

    default constructor in c++

---
+++ missprogconstr(noargs or def).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+	
+class employee{
+	private :
+	int age;
+	char *name;
+	
+	public:
+	employee()
+	{ 
+	age=0;
+	name="NULL";
+	}; 
+	/*employee(int a,char n[]):age(a),name(n)
+	{ }; */
+	 
+    
+    public :
+	void showdata(){
+		cout<<"The empl name is:"<<name<<endl;
+		cout<<"The empl age is:"<<age<<endl;
+	  }
+	};
+	
+int main()
+{
+	employee e1;
+/*employee e2(20,"Abdullah");*/
+	cout<<"First employee details are:"<<endl;
+	e1.showdata();
+	/*cout<<"Second employee details are:"<<endl;
+	e2.showdata(); */
+	
+	_getch();
+	return 0;
+}
+

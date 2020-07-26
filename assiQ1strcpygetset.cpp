94cf7483010e96158a8c7127ac641b634d2d6a77
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-27 04:15:40 +0500

    getter setter(esp for strcpy) added by me to assignment Q1

---
+++ assiQ1strcpygetset.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class Links {
+public:
+    char name[100];
+
+    Links() {
+        cout << "Links default constructor" << endl;
+    };
+    Links(char y[]) {
+        strcpy(name, y);
+        cout << "Links parameterized constructor having name: " << name << endl;
+    };
+//addition
+/*string getname(){
+	
+	return name;
+}
+*/
+
+};
+
+class WebPage {
+public:
+    double width, height;
+
+    WebPage() {
+        cout << "WebPage default constructor" << endl;
+    };
+    WebPage(double a, double b): width(a), height(b) {
+        cout << "WebPage parameterized constructor having width and height as: " << width << ", " << height << endl;
+    };
+
+    Links l1;
+    Links l2= {"www.gosomewhere.com"};
+
+};
+
+
+
+
+class WebSite {
+private:
+    char name[100];
+public:
+    WebSite() {
+        cout << "WebSite default constructor" << endl;
+    };
+    WebSite(char x[]) {
+        strcpy(name, x);
+        cout << "WebSite parameterized constructor having name: " << name << endl;
+    };
+
+    WebPage wp1;
+    WebPage wp2={640, 800};
+    WebPage wp3={400, 600};
+    WebPage wp4={1280, 720};
+    
+string getname(){
+	
+	return name;
+}  
+
+void setname(char x[]){
+	strcpy(name, x);
+}  
+
+};
+
+
+int main()
+{
+    WebSite web1("pakistan.com");
+    WebSite web2;
+    web2.setname("computer.com");
+    cout<<"now this is"<< web2.getname()<<endl;
+
+    return 0;
+}

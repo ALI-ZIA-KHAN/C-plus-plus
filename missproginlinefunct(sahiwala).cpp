c6c78066a553751351aafc5016f68b5330603fdc
ALI ZIA KHAN <azk443.443@gmail.com>
2020-07-27 21:24:01 +0500

    inline functions in oop

---
+++ missproginlinefunct(sahiwala).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+inline int area(int len,int wid){
+	
+	return len*wid;
+	
+}
+int main(){
+	
+	cout<<"The Area is equal to\t"<<area(10,20)<<"m^2"<<endl;
+}

ddc46f79d399e5f2672cb55ebca57d38160e341e
ALI ZIA KHAN <azk443.443@gmail.com>
2020-08-02 15:49:47 +0500

    Add files via upload

---
+++ midprac1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+string b="helllo pakistan";
+int main(){
+	cout<<b.length()<<endl;
+	cout<<b.size()<<endl;
+	
+	return 0;	
+}

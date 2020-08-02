6dd75647bbccae5c5fc0a9a72851b2a560137aba
ALI ZIA KHAN <azk443.443@gmail.com>
2020-08-02 16:16:04 +0500

    switch statements without break see result

--- midprac1.cpp
+++ midprac1.cpp
@@ -2,10 +2,34 @@
 #include<conio.h>
 #include<string.h>
 using namespace std;
-string b="helllo pakistan";
 int main(){
-	cout<<b.length()<<endl;
-	cout<<b.size()<<endl;
-	
-	return 0;	
+	char num='7';
+	switch(num){
+		case '6':
+			cout<<"The numbers is six"<<endl;
+			//break;
+		
+		case '5':
+		    cout<<"The number is five"<<endl;
+		    //break;
+		case '7':
+		    cout<<"The number is seven"<<endl;
+		//break;
+		case '8':
+		    cout<<"The number is eight"<<endl;		
+			//break;	
+		default:
+			cout<<"Your no. is invalid"<<endl;
+		}
+		
+		
+		cout<<"this was the result"<<endl;
+	return 0;
 }
+
+/*output
+The number is seven
+The number is eight
+Your no. is invalid
+this was the result
+*/

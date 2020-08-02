716408195c3d3588d9aa97c5d3b01b33e94a38d4
ALI ZIA KHAN <azk443.443@gmail.com>
2020-08-02 17:36:37 +0500

    factorial of a number using while loo

---
+++ midpracfactwhile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+
+
+int main()
+{
+	int num=6;
+	int fact=1;
+	int n=1;
+	while(n<=num){
+		
+		fact=fact*n;
+		
+	n++;	
+		
+	}
+cout<<"the result is"<<fact<<endl;
+return 0;
+}
